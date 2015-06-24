8cdd505a4ddbb9ed1e78d6187bd865a1d8da2398
mobiusbkst <tororosan@gmail.com>
2015-06-24 16:22:12 +0900

    Revert "Project Euler"
    This reverts commit 3a6f2cfe41b9671f4d9473179b43b30ca9ef57c0.

--- 003.rb
+++
@@ -1,8 +0,0 @@
-#Problem 3 「最大の素因数」
-#13195 の素因数は 5, 7, 13, 29 である.
-#600851475143 の素因数のうち最大のものを求めよ.
-#Problem 3 Largest prime factor
-#The prime factors of 13195 are 5, 7, 13 and 29.
-#What is the largest prime factor of the number 600851475143 ?
-require 'prime'
-puts 600851475143.prime_division.max[0]
--- 010.rb
+++
@@ -1,5 +0,0 @@
-# 問題: http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2010
-# https://projecteuler.net/problem=10
-# 考え方: 200万以下の素数を列挙して、それが配列の要素に入っているので配列の要素の和を求める
-require 'prime'
-puts Prime.each(2000000).inject(0){|sum,x| sum+x}
